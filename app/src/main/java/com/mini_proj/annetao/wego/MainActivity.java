48e1c176d35631a9d0c493bfeb9eefc2ff2803ba
Nightonke <Nightonke@outlook.com>
2016-07-07 20:43:06 +0800

    Bottom bar.

--- app/src/main/java/com/mini_proj/annetao/wego/MainActivity.java
+++ app/src/main/java/com/mini_proj/annetao/wego/MainActivity.java
@@ -1,13 +1,72 @@
 package com.mini_proj.annetao.wego;
 
+import android.support.annotation.IdRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItems(R.menu.main_bottom_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                switch (menuItemId) {
+                    case R.id.home:
+
+                        break;
+                    case R.id.discovery:
+
+                        break;
+                    case R.id.subscribe:
+
+                        break;
+                    case R.id.message:
+
+                        break;
+                    case R.id.me:
+
+                        break;
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                switch (menuItemId) {
+                    case R.id.home:
+
+                        break;
+                    case R.id.discovery:
+
+                        break;
+                    case R.id.subscribe:
+
+                        break;
+                    case R.id.message:
+
+                        break;
+                    case R.id.me:
+
+                        break;
+                }
+            }
+        });
+
+        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+        mBottomBar.mapColorForTab(1, 0xFF5D4037);
+        mBottomBar.mapColorForTab(2, "#7B1FA2");
+        mBottomBar.mapColorForTab(3, "#FF5252");
+        mBottomBar.mapColorForTab(4, "#FF9800");
     }
 }
