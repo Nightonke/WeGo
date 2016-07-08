29bc47da2b90d9e802107185aea31f391d5a61ac
Zyh <zyh_registry@163.com>
2016-07-08 09:55:20 +0800

    添加登录和地图工具目录

---
+++ app/src/main/java/com/mini_proj/annetao/wego/util/login/QQLoginSupporter.java
@@ -0,0 +1,7 @@
+package com.mini_proj.annetao.wego.util.login;
+
+/**
+ * Created by Administrator on 16/7/8.
+ */
+public class QQLoginSupporter {
+}
---
+++ app/src/main/java/com/mini_proj/annetao/wego/util/map/QQMapSupporter.java
@@ -0,0 +1,7 @@
+package com.mini_proj.annetao.wego.util.map;
+
+/**
+ * Created by Administrator on 16/7/8.
+ */
+public class QQMapSupporter {
+}
