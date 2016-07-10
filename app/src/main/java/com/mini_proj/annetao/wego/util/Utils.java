c13fdc0066613f6fc50448fc4cbd532d63d99266
Nightonke <Nightonke@outlook.com>
2016-07-10 11:23:15 +0800

    Main ui.

--- app/src/main/java/com/mini_proj/annetao/wego/util/Utils.java
+++ app/src/main/java/com/mini_proj/annetao/wego/util/Utils.java
@@ -3,7 +3,42 @@ package com.mini_proj.annetao.wego.util;
 /**
  * Created by huangweiping on 16/7/9.
  */
+
+import android.app.Activity;
+import android.view.Window;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * 工具单例类
+ */
 public class Utils {
+
+    /**
+     * MIUI下可以把状态栏的字体颜色改一下，以免看不清
+     * 在API23或以上，在style里面进行了设置
+     *
+     * @param activity
+     * @param darkMode
+     * @return
+     */
+    public static boolean setMiuiStatusBarDarkMode(Activity activity, boolean darkMode) {
+        Class<? extends Window> clazz = activity.getWindow().getClass();
+        try {
+            int darkModeFlag = 0;
+            Class<?> layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            extraFlagField.invoke(activity.getWindow(), darkMode ? darkModeFlag : 0, darkModeFlag);
+            return true;
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+        return false;
+    }
+
     private static Utils ourInstance = new Utils();
 
     public static Utils getInstance() {
