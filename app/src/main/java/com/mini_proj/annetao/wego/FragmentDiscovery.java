4e143d169a4af269d1835f83de8f652cd3a26079
Zyh <zyh_registry@163.com>
2016-07-11 11:51:28 +0800

    homefragment和discoveryframent已接入地图视图的切换

--- app/src/main/java/com/mini_proj/annetao/wego/FragmentDiscovery.java
+++ app/src/main/java/com/mini_proj/annetao/wego/FragmentDiscovery.java
@@ -6,13 +6,21 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.malinskiy.superrecyclerview.SuperRecyclerView;
 import com.mini_proj.annetao.wego.util.Utils;
+import com.mini_proj.annetao.wego.util.map.QQMapSupporter;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorListenerAdapter;
+import com.tencent.tencentmap.mapsdk.maps.MapView;
 
 import me.gujun.android.taggroup.TagGroup;
 
@@ -24,6 +32,9 @@ public class FragmentDiscovery extends Fragment {
     private TagGroup tagGroup;
     private RecyclerView recyclerView;
     private boolean shownMapView = false;
+    private RecyclerView listView;
+    private MapView mapView;
+    private QQMapSupporter qqMapSupporter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -60,12 +71,57 @@ public class FragmentDiscovery extends Fragment {
             tagGroup.getChildAt(i).setBackgroundResource(0);
             ((TextView)tagGroup.getChildAt(i)).setTextColor(ContextCompat.getColor(getContext(), R.color.tag));
         }
+        listView = (RecyclerView) messageLayout.findViewById(R.id.list_view);
+        LinearLayoutManager mManager = new LinearLayoutManager(getContext());
+        listView.setLayoutManager(mManager);
+
+
+        mapView = (MapView) messageLayout.findViewById(R.id.map_view);
+        qqMapSupporter = new QQMapSupporter(getActivity(),mapView,QQMapSupporter.QQ_MAP_TYPE_EXERCISES);
+        qqMapSupporter.initialMapView();
 
         return messageLayout;
     }
 
     public void toggleView() {
         shownMapView = !shownMapView;
+        if (shownMapView) {
+            YoYo.with(Techniques.BounceInUp).duration(700).playOn(mapView);
+            YoYo.with(Techniques.FadeOutUp)
+                    .withListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            super.onAnimationStart(animation);
+                            mapView.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            listView.setVisibility(View.GONE);
+                        }
+                    })
+                    .duration(300).playOn(listView);
+            if(qqMapSupporter.isMapLoaded) qqMapSupporter.updateExerciseMarker();
+
+        } else {
+            YoYo.with(Techniques.BounceInUp).duration(700).playOn(listView);
+            YoYo.with(Techniques.FadeOutUp)
+                    .withListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            super.onAnimationStart(animation);
+                            listView.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mapView.setVisibility(View.GONE);
+                        }
+                    })
+                    .duration(300).playOn(mapView);
+        }
     }
 
     public void chooseTag(String tagName) {
@@ -74,5 +130,36 @@ public class FragmentDiscovery extends Fragment {
 
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
 
 }
