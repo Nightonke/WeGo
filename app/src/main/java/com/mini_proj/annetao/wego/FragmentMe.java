c981e1f438940f436ff6c1bd6988ad64cd8da7e4
Nightonke <Nightonke@outlook.com>
2016-07-13 09:45:45 +0800

    Fragment me information.

--- app/src/main/java/com/mini_proj/annetao/wego/FragmentMe.java
+++ app/src/main/java/com/mini_proj/annetao/wego/FragmentMe.java
@@ -9,20 +9,53 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 /**
  * Created by huangweiping on 16/7/10.
  */
 public class FragmentMe extends Fragment implements View.OnClickListener{
 
+    private CircleImageView imageView;
+    private TextView userName;
+    private TextView birthday;
+    private TextView sex;
+    private TextView tags;
+
     private TextView signOutBtn;
+
+    private MaterialDialog dialog;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View messageLayout = inflater.inflate(R.layout.fragment_me, container, false);
+
+        imageView = (CircleImageView) messageLayout.findViewById(R.id.image);
+        if (User.getInstance().getAvatorUrl() != null) Picasso.with(getActivity()).load(User.getInstance().getAvatorUrl()).into(imageView);
+        userName = (TextView) messageLayout.findViewById(R.id.name);
+        userName.setText(User.getInstance().getName());
+        sex = (TextView) messageLayout.findViewById(R.id.sex);
+        String sexString = "男";
+        switch (User.getInstance().getGender()) {
+            case 0:
+                sexString = "男";
+                break;
+            case 1:
+                sexString = "女";
+                break;
+            case 2:
+                sexString = "不明";
+                break;
+        }
+        sex.setText(sexString);
+        tags = (TextView) messageLayout.findViewById(R.id.tags);
+        tags.setText(User.getInstance().getTagShowString());
+
         signOutBtn = (TextView) messageLayout.findViewById(R.id.sign_out);
         signOutBtn.setOnClickListener(this);
-
-
-
 
         return messageLayout;
     }
@@ -32,10 +65,21 @@ public class FragmentMe extends Fragment implements View.OnClickListener{
         switch (v.getId()) {
             case R.id.sign_out:
                 //TODO 登出数据处理
-                Toast.makeText(getActivity(),"signout",Toast.LENGTH_SHORT).show();
-                User.getInstance().setLogin(false);
-                Intent intent = new Intent(getActivity(),MainActivity.class);
-                startActivity(intent);
+                dialog = new MaterialDialog.Builder(getContext())
+                        .title("退出登录中")
+                        .content("请稍候...")
+                        .cancelable(false)
+                        .progress(true, 0)
+                        .show();
+                signOutBtn.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (dialog != null) dialog.dismiss();
+                        User.getInstance().setLogin(false);
+                        Intent intent = new Intent(getActivity(),MainActivity.class);
+                        startActivity(intent);
+                    }
+                }, 2000);
                 break;
 
         }
