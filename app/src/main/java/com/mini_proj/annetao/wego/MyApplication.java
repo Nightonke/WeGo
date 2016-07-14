d7d454b73cab40a8103a98e1ab3ce52bfc3ee53d
Nightonke <Nightonke@outlook.com>
2016-07-14 19:40:08 +0800

    把图片全切小了。

--- app/src/main/java/com/mini_proj/annetao/wego/MyApplication.java
+++ app/src/main/java/com/mini_proj/annetao/wego/MyApplication.java
@@ -3,7 +3,7 @@ package com.mini_proj.annetao.wego;
 import android.app.Application;
 import android.content.Context;
 
-//import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.LeakCanary;
 import com.zhy.http.okhttp.OkHttpUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -31,7 +31,7 @@ public class MyApplication extends Application
                 .build();
 
         OkHttpUtils.initClient(okHttpClient);
-//        LeakCanary.install(this);
+        LeakCanary.install(this);
         mContext = getApplicationContext();
     }
 
