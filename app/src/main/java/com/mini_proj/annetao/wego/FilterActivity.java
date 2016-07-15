afce48897585ccfa8bad494a9f80b896249ce388
Nightonke <Nightonke@outlook.com>
2016-07-15 10:39:03 +0800

    筛选页面UI（只有UI没有逻辑）。

--- app/src/main/java/com/mini_proj/annetao/wego/FilterActivity.java
+++ app/src/main/java/com/mini_proj/annetao/wego/FilterActivity.java
@@ -1,9 +1,15 @@
 package com.mini_proj.annetao.wego;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.InputType;
 import android.view.View;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.bumptech.glide.util.Util;
+import com.mini_proj.annetao.wego.util.Utils;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
@@ -22,7 +28,8 @@ public class FilterActivity extends BaseActivity
     private TitleLayout titleLayout;
     private TextView startTime;
     private TextView endTime;
-    private TextView people;
+    private TextView minPeople;
+    private TextView maxPeople;
     private TextView credit;
     private TextView distance;
 
@@ -39,13 +46,15 @@ public class FilterActivity extends BaseActivity
         titleLayout.setOnTitleActionListener(this);
         startTime = findView(R.id.start);
         endTime = findView(R.id.end);
-        people = findView(R.id.people);
+        minPeople = findView(R.id.min_people);
+        maxPeople = findView(R.id.max_people);
         credit = findView(R.id.credit);
         distance = findView(R.id.distance);
 
         findView(R.id.start_layout).setOnClickListener(this);
         findView(R.id.end_layout).setOnClickListener(this);
-        findView(R.id.people_layout).setOnClickListener(this);
+        findView(R.id.min_people_layout).setOnClickListener(this);
+        findView(R.id.max_people_layout).setOnClickListener(this);
         findView(R.id.credit_layout).setOnClickListener(this);
         findView(R.id.distance_layout).setOnClickListener(this);
 
@@ -70,7 +79,6 @@ public class FilterActivity extends BaseActivity
         );
         timePickerDialog.setTitle(isEndTime ? "结束时间" : "开始时间");
         timePickerDialog.show(getFragmentManager(), "");
-        setText();
     }
 
     @Override
@@ -97,7 +105,20 @@ public class FilterActivity extends BaseActivity
 
     @Override
     public void clickTitleEdit() {
-
+        // 如果需要，在这里加筛选逻辑
+        if (!startCalendar.before(endCalendar)) {
+            Utils.toastImmediately("活动开始时间必须早于活动结束时间");
+            return;
+        }
+        if (!"".equals(minPeople.getText().toString()) && !"".equals(maxPeople.getText().toString())) {
+            int min = Integer.valueOf(minPeople.getText().toString());
+            int max = Integer.valueOf(maxPeople.getText().toString());
+            if (min > max) {
+                Utils.toastImmediately("最少参与人数不能大于最多参与人数");
+                return;
+            }
+        }
+        finish();
     }
 
     @Override
@@ -115,22 +136,93 @@ public class FilterActivity extends BaseActivity
                 datePickerDialog.setTitle(isEndTime ? "结束时间" : "开始时间");
                 datePickerDialog.show(getFragmentManager(), "");
                 break;
-            case R.id.people_layout:
+            case R.id.min_people_layout:
+                new MaterialDialog.Builder(mContext)
+                        .title("最少参与人数")
+                        .content("")
+                        .inputType(InputType.TYPE_CLASS_NUMBER)
+                        .positiveText("确认")
+                        .negativeText("取消")
+                        .onAny(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                minPeople.setText(dialog.getInputEditText().getText().toString());
+                            }
+                        })
+                        .input("最少参与人数", minPeople.getText().toString(), new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
 
+                            }
+                        }).show();
+                break;
+            case R.id.max_people_layout:
+                new MaterialDialog.Builder(mContext)
+                        .title("最多参与人数")
+                        .content("")
+                        .inputType(InputType.TYPE_CLASS_NUMBER)
+                        .positiveText("确认")
+                        .negativeText("取消")
+                        .onAny(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                maxPeople.setText(dialog.getInputEditText().getText().toString());
+                            }
+                        })
+                        .input("最多参与人数", maxPeople.getText().toString(), new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
+
+                            }
+                        }).show();
                 break;
             case R.id.credit_layout:
+                new MaterialDialog.Builder(mContext)
+                        .title("信用要求")
+                        .content("")
+                        .inputType(InputType.TYPE_CLASS_NUMBER)
+                        .positiveText("确认")
+                        .negativeText("取消")
+                        .onAny(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                credit.setText(dialog.getInputEditText().getText().toString());
+                            }
+                        })
+                        .input("最多参与人数", credit.getText().toString(), new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
 
+                            }
+                        }).show();
                 break;
             case R.id.distance_layout:
+                new MaterialDialog.Builder(mContext)
+                        .title("信用要求")
+                        .content("")
+                        .inputType(InputType.TYPE_CLASS_NUMBER)
+                        .positiveText("确认")
+                        .negativeText("取消")
+                        .onAny(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                distance.setText(dialog.getInputEditText().getText().toString());
+                            }
+                        })
+                        .input("距离（米）", distance.getText().toString(), new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
 
+                            }
+                        }).show();
                 break;
         }
     }
 
     private void setText() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-        startTime.setText(simpleDateFormat.format(startCalendar.getTime()));
-        endTime.setText(simpleDateFormat.format(endCalendar.getTime()));
+        if (isEndTime) endTime.setText(simpleDateFormat.format(endCalendar.getTime()));
+        else startTime.setText(simpleDateFormat.format(startCalendar.getTime()));
     }
 
 }
